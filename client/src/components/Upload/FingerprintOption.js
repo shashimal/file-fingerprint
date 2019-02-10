1c8ed638fe07b11ceb6fb4189d113fdb7c7d688e
shashimal <shashimald@gmail.com>
2019-02-10 20:31:48 +0800

    Removed unused imports

--- client/src/components/Upload/FingerprintOption.js
+++ client/src/components/Upload/FingerprintOption.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Link ,Route ,withRouter} from 'react-router-dom';
+import { withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {selectFingerprintOption} from '../../actions/index';
 
